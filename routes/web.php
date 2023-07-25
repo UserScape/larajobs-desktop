67c88d044c9d0b52c7cfa299dd2a8aa4705ce2c6
Luke Towers <git@luketowers.ca>
2023-07-24 19:23:35 -0600

    Cache the fetched jobs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,36 +16,43 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/menubar/', function () {
-    $xml = simplexml_load_file('https://larajobs.com/feed');
+    $content = file_get_contents('https://larajobs.com/feed-test');
+    $cacheKey = 'larajobs-feed-contents.' . md5($content);
 
-    $jobs = [];
+    $jobs = Cache::remember($cacheKey, Carbon::now()->addWeek(1), function () use ($content) {
+        $jobs = [];
+        $xml = simplexml_load_string($content);
 
-    foreach($xml->channel->item as $item) {
-        $namespaces = $item->getNameSpaces(true);
-        $jobData = $item->children($namespaces['job']);
+        $jobs = [];
+        foreach ($xml->channel->item as $item) {
+            $namespaces = $item->getNameSpaces(true);
+            $jobData = $item->children($namespaces['job']);
 
-        $job = [
-            'title' => (string) $item->title,
-            'url' => (string) $item->link,
-            'published_at' => Carbon::parse((string) $item->pubDate),
-            'creator' => (string) $item->children($namespaces['dc'])->creator,
-            'location' => isset($jobData->location) ? (string) $jobData->location : null,
-            'type' => isset($jobData->job_type) ? (string) $jobData->job_type : null,
-            'salary' => isset($jobData->salary) ? (string) $jobData->salary : null,
-            'company' => isset($jobData->company) ? (string) $jobData->company : null,
-            'company_logo' => isset($jobData->company_logo) && pathinfo((string) $jobData->company_logo, PATHINFO_EXTENSION) ? (string) $jobData->company_logo : null,
-            'tags' => isset($jobData->tags) ? explode(',', (string) $jobData->tags) : [],
-        ];
+            $job = [
+                'title' => (string) $item->title,
+                'url' => (string) $item->link,
+                'published_at' => Carbon::parse((string) $item->pubDate),
+                'creator' => (string) $item->children($namespaces['dc'])->creator,
+                'location' => isset($jobData->location) ? (string) $jobData->location : null,
+                'type' => isset($jobData->job_type) ? (string) $jobData->job_type : null,
+                'salary' => isset($jobData->salary) ? (string) $jobData->salary : null,
+                'company' => isset($jobData->company) ? (string) $jobData->company : null,
+                'company_logo' => isset($jobData->company_logo) && pathinfo((string) $jobData->company_logo, PATHINFO_EXTENSION) ? (string) $jobData->company_logo : null,
+                'tags' => isset($jobData->tags) ? explode(',', (string) $jobData->tags) : [],
+            ];
 
-        foreach ($job as $key => &$value) {
-            if (is_string($value)) {
-                $value = html_entity_decode($value);
+            foreach ($job as $key => &$value) {
+                if (is_string($value)) {
+                    $value = html_entity_decode($value);
+                }
             }
-        }
-        unset($value);
+            unset($value);
 
-        $jobs[] = $job;
-    }
+            $jobs[] = $job;
+        }
+
+        return $jobs;
+    });
 
     return view('menubar.index', ['jobPosts' => $jobs]);
 })->name('menubar.index');
