88f87eb682c22c3776baf611126736f8faa4ee59
Dan Johnson <dan@try.be>
2023-08-03 00:04:06 +0100

    test: add more tests for inspecting notification contents
    We're swapping out the Notification facade with a mock using
    Laravel's built in facade helpers. This allows us to test
    that a notification was displayed, and allows us to inspect
    the contents of the notification.

--- tests/Feature/JobsPostedEventTest.php
+++ tests/Feature/JobsPostedEventTest.php
@@ -6,8 +6,10 @@ use App\Enums\FilterField;
 use App\Enums\FilterOperation;
 use App\Events\JobsPosted;
 use App\Models\Filter;
+use App\Models\JobCreator;
 use App\Models\JobPost;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Native\Laravel\Facades\Notification;
 use Tests\TestCase;
 
 class JobsPostedEventTest extends TestCase
@@ -21,35 +23,59 @@ class JobsPostedEventTest extends TestCase
 
     public function test_it_notifies_of_new_jobs()
     {
-        $job = JobPost::factory()->create();
+        JobPost::factory()->create();
 
-        $event = new JobsPosted();
+        Notification::shouldReceive('new->title->message->event->show')->once();
 
-        // Unfortunately we can't mock the NativePHP Notification
-        // facade, so instead we're just going to ensure that the
-        // jobs within the event contain what we're expecting to
-        // notify the user about.
-        $this->assertContains($job->id, $event->jobs->pluck('id'));
+        event(new JobsPosted());
+    }
+
+    public function test_the_notification_contains_the_expected_content_for_singular_job_alerts()
+    {
+        $creator = JobCreator::factory()->create(['name' => 'Beer Factory Ltd.']);
+        JobPost::factory()->for($creator, 'creator')->create([
+            'title' => 'Full-stack beer taster',
+            'salary' => '#100k'
+        ]);
+
+        Notification::shouldReceive('new')->andReturnSelf();
+        Notification::shouldReceive('title')->with("New job from Beer Factory Ltd.")->once()->andReturnSelf();
+        Notification::shouldReceive('message')->with('Full-stack beer taster: #100k')->once()->andReturnSelf();
+        Notification::shouldReceive('event')->once()->andReturnSelf();
+        Notification::shouldReceive('show')->once();
+
+        event(new JobsPosted());
+    }
+
+    public function test_the_notification_contains_the_expected_title_for_multiple_job_alerts()
+    {
+        $creator = JobCreator::factory()->create(['name' => 'Beer Factory Ltd.']);
+        JobPost::factory()->for($creator, 'creator')->create();
+        JobPost::factory()->for($creator, 'creator')->create();
+
+        Notification::shouldReceive('new')->andReturnSelf();
+        Notification::shouldReceive('title')->with("View the latest jobs")->once()->andReturnSelf();
+        Notification::shouldReceive('message')->with("2 new jobs available.")->once()->andReturnSelf();
+        Notification::shouldReceive('event')->once()->andReturnSelf();
+        Notification::shouldReceive('show')->once();
+
+        event(new JobsPosted());
     }
 
     public function test_it_only_notifies_of_jobs_meeting_notification_preference_criteria_when_filter_added()
     {
+        Notification::shouldReceive('new->title->message->event->show')->times(0);
+
         Filter::factory()->create([
             'field' => FilterField::Title,
             'operation' => FilterOperation::NotContains,
             'query' => 'Junior'
         ]);
 
-        $job = JobPost::factory()->create([
+        JobPost::factory()->create([
             'title' => 'Junior Developer'
         ]);
 
-        $event = new JobsPosted();
-
-        // Unfortunately we can't mock the NativePHP Notification
-        // facade, so instead we're just going to ensure that the
-        // jobs within the event contain what we're expecting to
-        // notify the user about.
-        $this->assertNotContains($job->id, $event->jobs->pluck('id'));
+        event(new JobsPosted());
     }
 }
